20d8ec7144a5f270c7cfe0eecaa9061533bf300e
ReddiJyothsnapriya <86516613+ReddiJyothsnapriya@users.noreply.github.com>
2021-06-27 22:17:43 +0530

    Add files via upload

---
+++ Bitwiseoddeven.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Bitwiseoddeven {
+
+	public static void main(String[] args) {
+		Scanner scanner = null;
+		int number;
+		
+		try {
+			scanner = new Scanner(System.in);
+                        System.out.print("Enter number:");
+			number = scanner.nextInt();
+			if(number%2 ==0)
+				System.out.println(number +" is an even number");
+			else
+				System.out.println(number +" is an odd number");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+Output:
+Enter number
+13
+number is an odd number
