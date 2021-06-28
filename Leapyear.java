668b0c0c89a97148224acd492932a1851b80eb18
ReddiJyothsnapriya <86516613+ReddiJyothsnapriya@users.noreply.github.com>
2021-06-28 21:06:06 +0530

    Update Leapyear.java

--- Leapyear.java
+++ Leapyear.java
@@ -15,12 +15,4 @@ System.out.println("specified year is not a leap year");
 }
 
 
-output 1:
-enter an year
-2020
-specifid year is a leap year
 
-output 2:
-enter a year
-2019
-specified year is not a leap year
