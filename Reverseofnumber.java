c0b3a49c160035f94a05ce314e50a0b7980d2300
ReddiJyothsnapriya <86516613+ReddiJyothsnapriya@users.noreply.github.com>
2021-06-29 21:15:54 +0530

    Add files via upload

---
+++ Reverseofnumber.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Reverseofnumber
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        int r;
+        int Sum=0;
+        if(n>0)
+        {
+            while(n>0)
+            {
+                r = n%10;
+                Sum=(Sum*10)+r;
+                n=n/10;
+            }
+            System.out.println(Sum);
+        }
+        else 
+        {
+            while(n<0)
+            {
+                r = n%10;
+            Sum=(Sum*10)+r;
+                n=n/10;
+            }
+            System.out.println(Sum);
+        }
+     }
+   }
+        
