b1b3b165c8112333d31fb966ac1c26fd3dc2afc7
ReddiJyothsnapriya <86516613+ReddiJyothsnapriya@users.noreply.github.com>
2021-06-29 10:09:02 +0530

    Add files via upload

---
+++ Rangeevenodd.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Rangeevenodd {
+    public static void main(String[] args) {
+        int n=50;
+        while(n>=10){
+            System.out.println(n);
+            n=n-2;
+}
+}
+}
